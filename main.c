f08794ceff0759ed21c85378bf1259b8ea703807
Beomseok Moon <jjdoop9@naver.com>
2018-10-16 17:10:34 +0900

    turbo

--- main.c
+++ main.c
@@ -3,10 +3,23 @@
 #include "func1.h"
 #include "func2.h"
 
+void turbo_on(void)
+{
+    printf("1234");
+}
+
+void turbo_off(Void)
+{
+    printf("5678");
+}
+
 int main (void)
 {
     func1();
     func2();
+
+    turbo_on();
+    turbo_off();
 
     return 0;
 }
